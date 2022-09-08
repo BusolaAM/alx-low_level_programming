c209b6cada7aa9d66e5eb321d0f496db18e89414
Busola Atinsola <busolaatinsola@gmail.com>
2022-09-08 15:39:42 -0700

    Put.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,11 @@
-#include <studio.h>
-
-
-
+#include <stdio.h>
 /**
+*main - prints Entry point
 *
-* * main - prints the size of various types 
-* *
-* * Return: 0 if exited properly, none-zero otherwise
-*
-* */
-
+*Return: Always 0 (successful)
+*/
 int main(void)
-
 {
-	
 	puts("\"Programming is like building a multilingual puzzle");
-	
 	return (0);
-
 }
