86543066b641740a8491f723b86e3065b8072452
Busola Atinsola <busolaatinsola@gmail.com>
2022-09-08 11:48:44 -0700

    Puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,23 +1,16 @@
 #include <studio.h>
 
-
-
-
-
 /**
- *  *
- *   * * main - prints the size of various types
- *    *
- *     * * Return: 0 if exited properly, non-zero otherwise
- *      *
- *       * */
-
+* main - prints the size of various types
+*
+*Description: using the main function
+*this programme prints programming is like building a multilingual puzzle
+* Return: 0
+*/
 int main(void)
-
 {
 
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
+^c
