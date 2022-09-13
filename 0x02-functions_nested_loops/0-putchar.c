0fb9fb283c73988572391da30424f80b804ec9a3
Busola Atinsola <busolaatinsola@gmail.com>
2022-09-13 09:04:51 -0700

    Functions, nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Program entry point
+ *
+ * Description: This is a program that will print the
+ * the message "_putchar" to the screen followed by a
+ * newline.
+* Return: Always 0 (If Successful)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+nclude "main.h"
+
+/**
+* print_sign - Determines if number is signed
+* @n: Integer number to be evaluated
+*
+* Description: This function prints the sign of a number
+*Return: 1 (> 0) , 0 (== 0) or -1 (< 0)
+*/
+int print_sign(int n)
+{
+		if (n < 0)
+		{
+			_putchar('-');
+			return (-1);
+		}
+		else if (n > 0)
+		{
+			_putchar('+');
+			return (1);
+		}
+		else
+		{
+			_putchar('0');
+			return (0);
+		}
+}
