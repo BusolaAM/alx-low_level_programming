aae5f9f2580b8c8351b5226cfaf1df1225eb569d
BusolaAM <busolaatinsola@gmail.com>
2022-10-25 00:39:23 +0100

    C - More Singly linked lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,18 +6,16 @@
  *
  * Return: number of nodes
  */
-
 size_t print_listint(const listint_t *h)
 {
-size_t len = 0;
-if (h)
+size_t len = 0;if (h)
 {
 while (h->next != NULL)
 {
-															len++;
-															printf("%d\n", h->n);
-															h = h->next;
-															}
+len++;
+printf("%d\n", h->n);
+h = h->next;	
+}
 len++;
 printf("%d\n", h->n);
 }
