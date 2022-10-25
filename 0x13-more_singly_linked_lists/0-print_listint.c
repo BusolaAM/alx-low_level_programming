a439fd7c212cfcad5189d8ed04809efcc73c9d6e
BusolaAM <busolaatinsola@gmail.com>
2022-10-25 13:40:58 +0100

    C - More Singly linked lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "lists.h"
 
 /**
@@ -8,14 +9,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t nnodes = 0;
+	size_t counter = 0;
 
-	while (h != NULL)
+	while (h)
 	{
 		printf("%d\n", h->n);
+		++counter;
 		h = h->next;
-		nnodes++;
+		
 	}
-	return (nnodes);
+	return (counter);
 }
-
